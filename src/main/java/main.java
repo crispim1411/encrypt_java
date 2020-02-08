f51b4701399177fff4fa4efb8bf8a91c8aa02aa0
Rodrigo Crispim <crispim1411@gmail.com>
2020-02-08 00:33:08 +0000

    cifra e decifra
    cifra e decifra feito dentro do código com variáveis internas

---
+++ src/main/java/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author crispim
+ */
+public class main {
+    public static void main(String[] args) {
+        String originalString = "howtodoinjava.com";
+        String secretKey = "macarrao";
+     
+        String encryptedString = AES.encrypt(originalString, secretKey) ;
+        String decryptedString = AES.decrypt(encryptedString, secretKey) ;
+
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+    }
+    
+}
